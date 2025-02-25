e7fbca6cfa1fda6a02e7ee9684f84dec1361948e
axel f <axel.figerou@gmail.com>
2025-02-25 18:17:25 +0100

    ajout fichiers docker

--- env/environment.ts
+++ env/environment.ts
@@ -1,6 +1,11 @@
 export const environment = {
     production: false,
-    baseUrl: "http://localhost:8080",
-    PORT: 8000,
-    MONGODB_URL: "mongodb://localhost:27017/Coordi_f_b"
+    baseUrl: process.env.BASE_URL ||  "http://localhost:8000/",
+    PORT: process.env.PORT ||  8000,
+    MONGODB_URL: process.env.MONGODB_URL ||  "mongodb://localhost:27017/MacroMate",
+    JWT_SECRET: process.env.JWT_SECRET ||  "secret123",
+    Node_ENV: process.env.NODE_ENV ||  "development",
+    googleClientID: process.env.GOOGLE_CLIENT_ID ||  "865906994045-50617hi1ebaqh2khsqiho0oegkenhm19.apps.googleusercontent.com",
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET ||  "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",
+    SESSION_SECRET: process.env.SESSION_SECRET ||  "supersecret123",
 };
