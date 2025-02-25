0a1366bff929708845f1cbbd071faffc19b11f38
axel f <axel.figerou@gmail.com>
2025-02-25 19:29:12 +0100

    ajout fichiers docker

--- middlewares/mongoDB.ts
+++ middlewares/mongoDB.ts
@@ -3,7 +3,7 @@ import { environment } from "../env/environment";
 
 const connectMongoDB = async () => {
   try {
-    const conn = await mongoose.connect(environment.MONGODB_URL || "");
+    const conn = await mongoose.connect(environment.MONGODB_URL + '?authSource=admin');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error: any) {
     console.error(`Error: ${error.message}`);
