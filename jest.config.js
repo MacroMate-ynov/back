da71f91ada11df8f717fed8fd0cbc3894638d602
Axel Figerou <axel.figerou@pole-emploi.fr>
2025-03-20 01:25:08 +0100

    fix coverage

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   testEnvironment: "node",
-  testTimeout: 60000,  // 30 secondes
   preset: "ts-jest",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   transform: {
